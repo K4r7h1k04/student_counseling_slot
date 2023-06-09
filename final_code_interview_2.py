53cecfbbe00f181b74bee3366a7b563192da5b8e
karthik <124885785+K4r7h1k04@users.noreply.github.com>
2023-06-09 16:26:10 +0530

    Add files via upload
    Find the student details who got slot on the specific counselling date
    Description
    Write a program to find the student details who got slot on a specific counselling date based 
    on the student’s Cutoff marks, Age, HSC and SSLC marks.
    Input:
    1. The number of slots available. (Integer)
    2. List of slot details:
    a. Date - Counselling date (Integer)
    b. Slot ID - Unique ID of the slot (Integer)
    c. Number of seats - Number of seats available in the slot (Integer)
    3. Number of students applied for the counselling (Integer)
    4. List of student details:
    d. Student ID - Unique ID of the student (Integer)
    e. CutOff Mark - Cutoff Mark of the student (Integer)
    f. Age - Student age (Integer)
    g. HSC Mark - HSC mark of the student (Integer)
    h. SSLC Mark - SSLC mark of the student (Integer)
    5. Date - Have to find the student details who get counselling on this date (Integer)
    Output:
    6. Identify the student details who got counselling on the specific date and print the 
    details as pipe separated (Date|StudentID|SlotID)
    Constraints:
    7. In one day, there can be more than one slot.
    8. In one slot, there can be more than one seat.
    9. The number of slots and seats will be equal or greater than the number of students.

---
+++ final_code_interview_2.py
@@ -0,0 +1,53 @@
+def findCounselling(no_of_slots, no_of_students, slots, students, date):
+    # Sort the students based on cutoff marks, age, HSC marks, and SSLC marks in descending order
+    students.sort(key=lambda x: (-x[1], -x[2], -x[3], -x[4]))
+
+    # Initialize a dictionary to store the allocated slots
+    allocated_slots = {}
+
+    # Iterate over the slots and allocate seats to the students
+    for slot in slots:
+        slot_date = slot[0]
+        slot_id = slot[1]
+        no_of_seats = slot[2]
+        if slot_date not in allocated_slots:
+            allocated_slots[slot_date] = {}
+        allocated_slots_on_date = allocated_slots[slot_date]
+        for i in range(1, no_of_seats + 1):
+            if len(students) == 0:
+                break
+            student = students.pop(0)
+            student_id = student[0]
+            allocated_slots_on_date[student_id] = slot_id
+
+    # Print the details of students who got the slot on the specific date
+    if date in allocated_slots:
+        allocated_slots_on_date = allocated_slots[date]
+        for student in allocated_slots_on_date:
+            slot_id = allocated_slots_on_date[student]
+            print(f"{date}|{student}|{slot_id}")            
+# Get slots
+no_of_slots = int(input())
+slots = []
+for _ in range(no_of_slots):
+    date = int(input()) # Slot date
+    slot_id = int(input()) # Slot id
+    no_of_seats = int(input()) # Number of seats available in the slot
+    slot = [date, slot_id, no_of_seats]
+    slots.append(slot)
+    
+# Get students
+no_of_students = int(input())
+students = []
+for _ in range(no_of_students):
+    stud_id = int(input()) # Student ID
+    cutoff_mark = int(input()) # Student cutoff mark
+    age = int(input()) # Student age
+    hsc_mark = int(input()) # Student HSC mark
+    sslc_mark = int(input()) # Student SSLC mark
+    student = [stud_id, cutoff_mark, age, hsc_mark, sslc_mark]
+    students.append(student)  
+# Get date
+date = int(input())
+
+findCounselling(no_of_slots, no_of_students, slots, students, date)
