d130a7391ec7f80364b016abaaac858118cd666a
karthik <124885785+K4r7h1k04@users.noreply.github.com>
2023-06-26 09:32:15 -0500

    Update gittesting.py

--- gittesting.py
+++ gittesting.py
@@ -4,4 +4,5 @@ code .                  =========opening vs code
 git add .\<file name>   =========adding new file or modified file to tracking
 git status              =========checking tracked file
 git commit -m " msg"    =========commiting the files to push to repos
-git push                =========pushing to repos                """
+git push                =========pushing to repos     
+git pull                =========pulling to local if any editing done in repos it will easily update it in local                 """
